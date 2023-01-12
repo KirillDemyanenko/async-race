e93c731b64fac8b65bc09a811be43eb546aa97e4
KirillDemyanenko <demyanenkokg@gmail.com>
2023-01-12 23:24:27 +0300

    refactor: rewrite methods

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import './style.scss';
 import Garage from './model/garage';
-import {drawCar} from './view/view';
-const a: Garage = new Garage()
-async function start () {
-    const cars = await a.getCars();
+// import {drawCar} from './view/view';
+import {Car, CarData} from './types';
+
+async function start() {
+    // const dC: CarData = {name:'gfdg', color:'#670c0c'}
+    // const car: Car = await Garage.updateCar(dC, 2)
+            const cars: Car[] = await Garage.getCars();
+    //     for (let i = 0; i < 7; i++) {
+    //         drawCar(cars[i].color, i);
+    //     }
+    // await Garage.deleteCar(5)
     setTimeout(() => console.log(cars), 2000)
 }
-(async () => await start())()
-drawCar('#c4ff12', 1)
-drawCar('#c00dd5', 2)
-drawCar('#1144b0', 3)
-drawCar('#de0fa4', 4)
-drawCar('#59550b', 5)
-drawCar('#669188', 6)
-drawCar('#620e4e', 7)
+
+(async () => await start())();
+// drawCar('#c4ff12', 1);
+// drawCar('#c00dd5', 2);
+// drawCar('#1144b0', 3);
+// drawCar('#de0fa4', 4);
+// drawCar('#59550b', 5);
+// drawCar('#669188', 6);
+// drawCar('#620e4e', 0);
--- src/model/garage.ts
+++ src/model/garage.ts
@@ -1,10 +1,10 @@
-import {Car} from '../types';
+import {Car, CarData} from '../types';
 
 class Garage {
 
     /* Returns json data about cars in a garage. */
-    async getCars(page: number = 0, limit: number = 0): Promise<Car[]> {
-        return await fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`)
+    static getCars(page: number = 0, limit: number = 0): Promise<Car[]> {
+        return fetch(`http://127.0.0.1:3000/garage?_page=${page}&_limit=${limit}`)
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(response.statusText);
@@ -14,39 +14,52 @@ class Garage {
     }
 
     /* Returns json data about specified car. */
-    static getCar(id: number) {
+    static getCar(id: number): Promise<Car> {
         return fetch(`http://127.0.0.1:3000/garage/${id}`)
-            .then((response) => response.json());
+            .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response.json() as Promise<Car>;
+                }
+            );
     }
 
     /* Creates a new car in a garage. */
-    static async createCar(data: Car) {
-        return await fetch('http://127.0.0.1:3000/garage', {
+    static createCar(data: CarData): Promise<Car> {
+        return fetch('http://127.0.0.1:3000/garage', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(data),
         })
-            .then((response) => response.json());
+            .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response.json() as Promise<Car>;
+                }
+            );
     }
 
     /* Delete specified car from a garage */
-    static deleteCar(id: number) {
+    static deleteCar(id: number): void {
         fetch(`http://127.0.0.1:3000/garage/${id}`, {
             method: 'DELETE',
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((response) => {
+                response.ok ? console.log('DELETED') : console.log('NOT DELETED');
+            })
+            .catch(error => console.log(error.text));
     }
 
     /* Updates attributes of specified car. */
-    static updateCar(data: Car, id: number) {
-        fetch(`http://127.0.0.1:3000/garage/${id}`, {
+    static updateCar(data: CarData, id: number): Promise<Car> {
+        return fetch(`http://127.0.0.1:3000/garage/${id}`, {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(data),
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((response) => response.json());
     }
 }
 
