26046cb038455d6d89bd531bb7a8540af270bec0
KirillDemyanenko <demyanenkokg@gmail.com>
2023-01-15 14:58:46 +0300

    fix: generate page

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,37 +1,32 @@
-import { drawCar, generateHeader } from '../view/view';
-import { Car, CarData } from '../types';
+import {Car, CarData, Winner, WinnerData} from '../types';
 import Garage from '../model/garage';
+import {generateHeader, drawCar} from '../view/view'
+import Winners from '../model/winners';
+import winners from '../model/winners';
 
 async function buttonCreateNewCar(form: ParentNode) {
-  const arr = Array.from(form.children) as Array<HTMLElement>;
-  const carData: CarData = {
-    name: (arr[0].lastChild as HTMLInputElement).value,
-    color: (arr[1].lastChild as HTMLInputElement).value,
-  };
-  await Garage.createCar(carData);
+    const arr = Array.from(form.children) as Array<HTMLElement>;
+    const carData: CarData = {
+        name: (arr[0].lastChild as HTMLInputElement).value,
+        color: (arr[1].lastChild as HTMLInputElement).value,
+    };
+    await Garage.createCar(carData);
 }
 
 function buttonUpdateCar() {
-  console.log('UPDATE');
+    console.log('UPDATE');
 }
 
 export async function start(): Promise<void> {
-  await fetch('http://127.0.0.1:3000', { method: 'HEAD' })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Server is not available!');
-      }
-      generateHeader();
-    })
-    .catch(() => console.log('Server is not available!'));
-  // const cars: Car[] = await Garage.getCars();
-  // setTimeout(() => console.log(...cars), 2000);
-  // setTimeout(() => {
-  //   for (let i = 0; i <= 7; i += 1) {
-  //     drawCar(cars[i].color, i);
-  //     console.log(cars[i]);
-  //   }
-  // }, 3000);
+    await fetch('http://127.0.0.1:3000', {method: 'HEAD'})
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Server is not available!');
+            }
+            generateHeader();
+            drawCar('#fff', 1)
+        })
+        .catch(() => console.log('Server is not available!'));
 }
 
-export { buttonUpdateCar, buttonCreateNewCar };
+export {buttonUpdateCar, buttonCreateNewCar};
