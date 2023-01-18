33f36d238753108ebca7d82c6694ce6bf5ac1053
KirillDemyanenko <demyanenkokg@gmail.com>
2023-01-18 23:40:35 +0300

    feat: add CarDrive interface

--- src/types.ts
+++ src/types.ts
@@ -1,38 +1,43 @@
 export interface CarData {
-  name: string,
-  color: string
+    name: string,
+    color: string
 }
 
 export interface Car {
-  name: string,
-  color: string,
-  id: number
+    name: string,
+    color: string,
+    id: number
 }
 
 export interface Winner {
-  'id': number,
-  'wins': number,
-  'time': number
+    'id': number,
+    'wins': number,
+    'time': number
 }
 
 export interface WinnerData {
-  'wins': number,
-  'time': number
+    'wins': number,
+    'time': number
 }
 
 export interface CarsAPI {
-  'id': string,
-  'name': string,
-  'cyrillic-name': string,
-  'models': number
+    'id': string,
+    'name': string,
+    'cyrillic-name': string,
+    'models': number
 }
 
 export interface CarsAPIModel {
-  'id': string,
-  'name': string,
-  'cyrillic-name': string,
-  'class': string,
-  'year-from': number,
-  'year-to': number | null,
-  'generations': number
+    'id': string,
+    'name': string,
+    'cyrillic-name': string,
+    'class': string,
+    'year-from': number,
+    'year-to': number | null,
+    'generations': number
+}
+
+export interface CarDrive {
+    'velocity': number,
+    'distance': number
 }
